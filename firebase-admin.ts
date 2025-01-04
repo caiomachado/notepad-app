713d81db1e8f765648bb81c5b712b5a3af77d622
Caio Machado <caiomachado1102@gmail.com>
2025-01-04 00:53:21 +0000

    fixed import issue

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,13 +1,12 @@
-import { App, cert, getApp, getApps, initializeApp } from "firebase-admin/app";
+import { App, cert, getApp, getApps, initializeApp, ServiceAccount } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-
-const serviceKey = require('./service_key.json');
+import serviceKey from './service_key.json';
 
 let app: App;
 
 if (getApps().length === 0) {
     app = initializeApp({
-        credential: cert(serviceKey),
+        credential: cert(serviceKey as string | ServiceAccount),
     });
 } else {
     app = getApp();
